567383f6c8abf62607dc643983777e3828c196cb
User54681 <anzolotylh@gmail.com>
2023-11-20 21:02:22 +0300

    Добавьте файлы проекта.

---
+++ че-то на четверг.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin; 
+
+class Circle {
+private:
+	int x, y, R;
+	std::string colour;
+public:
+	void SetX(int valueX){
+		x = valueX;
+	}
+	void SetY(int valueY) {
+		y = valueY;
+	}
+	void SetR(int valueR) {
+		R = valueR;
+	}
+};
+
+int main()
+{
+	
+}
